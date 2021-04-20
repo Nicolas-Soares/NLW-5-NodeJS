cb14335eb517d890d4d4636b89058a7250486632
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-04-20 12:52:38 -0300

    Revert "Iniciando com banco de dados"
    This reverts commit cc8506547e1aa04198ed272d768b1f4d4cd8a579.

--- src/repositories/SettingsRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { Setting } from "../entities/Setting";
-
-@EntityRepository(Setting)
-class SettingsRepository extends Repository <Setting>{}
-
-export { SettingsRepository }
